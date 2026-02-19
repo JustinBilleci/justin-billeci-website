8903e0085c8ad19aa44f7b7505c2787223cdb87f
Justin Billeci <justinbilleci15@gmail.com>
2026-02-18 23:05:22 -0500

    minor fixes

--- src/components/Main.svelte
+++ src/components/Main.svelte
@@ -16,11 +16,11 @@
     ];
 
     let benefits = [
-    {name: 'a self taught developer', 
+    {name: 'A self taught developer', 
     description: "I learned to code using free online resources and absolutely fell in love with the process, there's nothing I'd rather do."},
-    {name: 'a web design master', 
+    {name: 'A web design master', 
     description: "I have a strong foundation and excellent work experience with Responsive Web Design, I view this as the strongest part of my skillset."},
-    {name: 'a great communicator', 
+    {name: 'A great communicator', 
     description: "Communication is just as important as coding ability in my eyes, so I make an effort to communicate as effectively and clearly as possible."},
     ]
 
@@ -28,26 +28,40 @@
 
 <main class="flex flex-col flex-1 p-4">
     <section id="introPage" class="grid grid-cols-1 lg:grid-cols-2 gap-10 py-8 sm:py-14">
-        <div class="flex flex-col lg:justify-center text-center lg:text-left gap-6 md:gap-8 lg:gap-10">
-            <h2 class="font-semibold text-4xl sm:text-5xl md:text-6xl"> 
-                Hi! I'm <span class="poppins text-green-500">Justin</span> Billeci <br/>Full Stack
-                <span class="poppins text-green-500">Devloper</span>
+        <div class="flex flex-col lg:justify-center text-center lg:text-left ">
+            <!-- Heading Text Start -->
+            <h2 class="font-semibold text-4xl sm:text-5xl md:text-6xl pb-10 md:pb-12 lg:pb-14"> 
+                Hi! I'm <span class="poppins text-green-500">Justin Billeci</span> <br/>Full Stack
+                <span class="poppins text-green-500">Developer</span>
             </h2>
-            <p class="text-base sm:text-lg md:text-xl">
-                My <span class="poppins text-green-500">favorite tech</span> includes JavaScript (React, Svelte, or Cold Fusion), TailwindCSS, Node.js and much more!
+            <!-- Heading text End -->
+
+            <!-- Details Start -->
+            <p class="text-base sm:text-lg md:text-xl pb-4 md:pb-6">
+                My <span class="poppins text-green-500">Top Skills</span> include <span class="font-semibold poppins">Project Overview</span>, <span class="font-semibold poppins">Project Management</span>, <span class="font-semibold poppins">Team Management</span>, <span class="font-semibold poppins">Client Relations</span>, and <span class="font-semibold poppins">Database Management</span>.
             </p>
+
+            <p class="text-base sm:text-lg md:text-xl pb-4 md:pb-6">
+                My <span class="poppins text-green-500">Top Programming Skills</span> include JavaScript (<span class="font-semibold poppins">React</span>, <span class="font-semibold poppins">Svelte</span>, and <span class="font-semibold poppins">Express.js</span>), <span class="font-semibold poppins">TailwindCSS</span>, and <span class="font-semibold poppins">PostgreSQL</span>.
+            </p>
+            <!-- Details End -->
+
+            <!-- Get In Touch Button Start -->
             <a href="https://www.linkedin.com/in/justinbilleci/" target="_blank" class="blueShadow mx-auto lg:mr-auto text-base sm:text-lg md:text-xl poppins relative overflow-hidden px-6 py-3 group rounded-full bg-white text-slate-950 cursor-pointer">
                 <div class="absolute top-0 right-full w-full h-full bg-green-700 opacity-20 group-hover:translate-x-full z-0 duration-200">
                 </div>
                 <h4 class="relative z-40">Get in touch &rarr;</h4>
             </a>
+            <!-- Get In Touch Button End -->
         </div>
 
+        <!-- Cover Image Start -->
         <div class="relative shadow-2xl grid place-items-center">
             <img src={"images/profile.jpeg"} 
-                alt="picture of me :)" 
+                alt="Justin Billeci Cover Pic" 
                 class="object-cover z-20 max-h-[70vh] rounded-full"/>
         </div>
+        <!-- Cover Image End -->
     </section>
 
     <section id="projects" class="py-20 lg:py-32 flex flex-col gap-24">
